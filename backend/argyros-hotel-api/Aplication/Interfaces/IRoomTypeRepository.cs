3e989388faaca8eb53cfa3211d1bb45f77d93c7d
andresaragonguerrero <andresaragonguerrero@gmail.com>
2026-03-22 10:41:58 +0100

    Creación de la interfaz correspondiente a el tipo de habitación y su persistencia de datos

---
+++ backend/argyros-hotel-api/Aplication/Interfaces/IRoomTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using ArgrosHotel.Domain.Hotel;
+
+namespace argyros_hotel_api.Aplication.Interfaces
+{
+    public interface IRoomTypeRepository
+    {
+        // Este repositorio se encarga de gestionar los tipos de habitaciones
+        // EL PROYECTO NO REQUIERE DE FUNCIONALIDADES DE CREACIÓN, ACTUALIZACIÓN, ETC PORQUE NO ES UNA APLICACIÓN DE ADMINISTRACIÓN DE HOTEL
+        public interface IRoomTypeRepository
+        {
+            Task<RoomType?> GetByIdAsync(Guid id);
+            Task<IEnumerable<RoomType>> GetAllAsync();
+        }
+    }
+}
---
+++ backend/argyros-hotel-api/Infrastructure/Repositories/InMemoryRoomTypeRepository.cs
@@ -0,0 +1,32 @@
+﻿using ArgrosHotel.Domain.Hotel;
+using argyros_hotel_api.Aplication.Interfaces;
+using System.Text.Json;
+
+namespace argyros_hotel_api.Infrastructure.Repositories
+{
+    // Implementación básica en memoria del repositorio de tipos de habitación
+    public class InMemoryRoomTypeRepository : IRoomTypeRepository
+    {
+        private readonly List<RoomType> _roomTypes;
+
+        public InMemoryRoomTypeRepository(string jsonFilePath)
+        {
+            if (!File.Exists(jsonFilePath))
+                _roomTypes = new List<RoomType>();
+            else
+            {
+                
+                var json = File.ReadAllText(jsonFilePath);
+                _roomTypes = JsonSerializer.Deserialize<List<RoomType>>(json) 
+                                ?? new List<RoomType>();
+                
+            }
+        }
+
+        public Task<RoomType?> GetByIdAsync(Guid id) => 
+            Task.FromResult(_roomTypes.FirstOrDefault(rt => rt.Id == id));
+
+        public Task<IEnumerable<RoomType>> GetAllAsync() => 
+            Task.FromResult(_roomTypes.AsEnumerable());
+    }
+}
