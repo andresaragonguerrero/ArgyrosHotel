5b7e6029ab755865d6dcb7be62769577845b3a8f
andresaragonguerrero <andresaragonguerrero@gmail.com>
2026-03-22 11:40:19 +0100

    Se creó la interfaz relacionada con las reservas de los servicios de habitación

---
+++ backend/argyros-hotel-api/Aplication/Interfaces/IBookingServiceRepository.cs
@@ -0,0 +1,11 @@
+﻿using argyros_hotel_api.Domain.Bookings;
+
+namespace argyros_hotel_api.Aplication.Interfaces
+{
+    public interface IBookingServiceRepository
+    {
+        Task<BookingService?> GetByIdAsync(Guid id);
+        Task<IEnumerable<BookingService>> GetAllAsync();
+        Task<IEnumerable<BookingService>> GetByBookingIdAsync(Guid bookingId);
+    }
+}
