fee31360fa0b18b3446c444068d74f495aecb2cc
andresaragonguerrero <andresaragonguerrero@gmail.com>
2026-03-22 11:38:10 +0100

    Creación de la persistencia de datos relacionada con los servicios de habitación

---
+++ backend/argyros-hotel-api/Infrastructure/Repositories/InMemoryServiceRepository.cs
@@ -0,0 +1,30 @@
+﻿using argyros_hotel_api.Aplication.Interfaces;
+using argyros_hotel_api.Domain.Hotel;
+using System.Text.Json;
+
+namespace argyros_hotel_api.Infrastructure.Repositories
+{
+    public class InMemoryServiceRepository : IServiceRepository
+    {
+        private readonly List<Service> _services;
+
+        public InMemoryServiceRepository(string jsonFilePath)
+        {
+            if (!File.Exists(jsonFilePath))
+                _services = new List<Service>();
+            else
+            {
+                var json = File.ReadAllText(jsonFilePath);
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                _services = JsonSerializer.Deserialize<List<Service>>(json, options)
+                            ?? new List<Service>();
+            }
+        }
+
+        public Task<Service?> GetByIdAsync(Guid id) =>
+            Task.FromResult(_services.FirstOrDefault(s => s.Id == id));
+
+        public Task<IEnumerable<Service>> GetAllAsync() =>
+            Task.FromResult(_services.AsEnumerable());
+    }
+}
