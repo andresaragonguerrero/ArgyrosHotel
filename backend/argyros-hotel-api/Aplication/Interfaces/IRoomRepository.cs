22d2e5679a34a044cf783bd24a4fd253037d41fd
andresaragonguerrero <andresaragonguerrero@gmail.com>
2026-03-22 11:23:19 +0100

    Creación de la persistencia de datos relacionada con las habitaciones

--- backend/argyros-hotel-api/Aplication/Interfaces/IRoomRepository.cs
+++ backend/argyros-hotel-api/Aplication/Interfaces/IRoomRepository.cs
@@ -1,10 +1,7 @@
-﻿using argyros_hotel_api.Domain.Bookings;
-using argyros_hotel_api.Domain.Hotel;
+﻿using argyros_hotel_api.Domain.Hotel;
 
 namespace argyros_hotel_api.Aplication.Interfaces
-{
-    // En esta interfaz necesitaré obtener las habitaciones disponibles,
-    // obtenerlas por tipos y por asignación en las reservas
+{    
     public interface IRoomRepository
     {
         Task<Room?> GetByIdAsync(Guid id);
