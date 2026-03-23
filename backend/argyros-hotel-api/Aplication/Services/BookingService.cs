b2bc4240cb15dd950bf0295ba52a389b901ea239
andresaragonguerrero <andresaragonguerrero@gmail.com>
2026-03-23 13:28:54 +0100

    Escritura del pseudocódigo del servicio encargado de la disponibilidad de habitaciones

--- backend/argyros-hotel-api/Aplication/Services/BookingService.cs
+++ backend/argyros-hotel-api/Aplication/Services/BookingService.cs
@@ -1,5 +1,6 @@
 ﻿namespace argyros_hotel_api.Aplication.Services
 {
+    // Archivo encargado de orquestar los servicios relacionados con las reservas 
     public class BookingService
     {
     }
--- backend/argyros-hotel-api/Domain/Services/AvailabilityService.cs
+++ backend/argyros-hotel-api/Domain/Services/AvailabilityService.cs
@@ -1,6 +1,19 @@
 ﻿namespace argyros_hotel_api.Domain.Services
 {
+    // Servicio encargado de manejar la lógica relacionada con la disponibilidad de las habitaciones
+    // Verifica la disponibilidad del tipo de habitación solicitada para las fechas dadas, considera reservas existentes
     public class AvailabilityService
     {
+        // Recupera el rango de las fechas de la reserva:
+        // InMemoryBookingRepository -> GetBookingsByRoomTypeAndDateRangeAsync(Guid roomTypeId, DateTime startDate, DateTime endDate)
+
+        // Verifica la disponibilidad comparando el número de reservas existentes con la cantidad total de habitaciones de ese tipo:
+        // InMemoryRoomRepository ->  GetByRoomTypeIdAsync(Guid roomTypeId)
+
+        // Verificar si se produce un solapamiento de fechas entre las reservas existentes y las fechas solicitadas
+
+        // Devuelve el resultado indicando si hay habitaciones disponibles
+        // En caso de que haya, indicará también el número de ellas
+        // De lo contrario, indicará que no hay disponibilidad
     }
 }
