4c9e803691befab839081c2c8ec9d8ddf0954e63
andresaragonguerrero <andresaragonguerrero@gmail.com>
2026-03-23 18:13:42 +0100

    Se implementó la lógica al servicio encargado de calcular el precio base de la reserva de la habitación

--- backend/argyros-hotel-api/Domain/Services/PricingService.cs
+++ backend/argyros-hotel-api/Domain/Services/PricingService.cs
@@ -1,6 +1,28 @@
 ﻿namespace argyros_hotel_api.Domain.Services
 {
+    // Servicio encargado de la lógica relacionada con el cálculo del precio de las reservas
+    // NO se incluirá la lógica del cálculo de impuestos, descuentos o promociones:
+    // esta funcionalidad se delegará a otros servicios especializados (DiscountService, TaxService)
     public class PricingService
     {
-    }
+        public decimal CalculateBasePrice(
+                    RoomType roomType,
+                    DateTime startDate,
+                    DateTime endDate)
+        {
+            if (startDate >= endDate)
+                throw new ArgumentException("Invalid date range");
+
+            // Calcular tiempo de la estancia en noches
+            var nights = (endDate - startDate).Days;
+
+            if (nights <= 0)
+                throw new ArgumentException("Stay must be at least one night");
+
+            // Calcular precio base
+            var totalPrice = nights * roomType.BasePrice;
+
+            return totalPrice;
+        }// CalculateTotalPrice method.end
+    } // PricingService class.end
 }
