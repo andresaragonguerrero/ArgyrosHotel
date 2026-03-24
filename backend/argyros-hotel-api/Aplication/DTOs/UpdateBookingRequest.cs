8c66bb58719fd92f58a37bf6d87b70a46ad5bd01
andresaragonguerrero <andresaragonguerrero@gmail.com>
2026-03-24 13:45:21 +0100

    Se agregó el DTO de la actualización de las reservas

---
+++ backend/argyros-hotel-api/Aplication/DTOs/UpdateBookingRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace argyros_hotel_api.Aplication.DTOs
+{
+    public class UpdateBookingRequest
+    {
+        public Guid UserId { get; set; }
+
+        public Guid RoomTypeId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int NumberOfGuests { get; set; }
+    }
+}
