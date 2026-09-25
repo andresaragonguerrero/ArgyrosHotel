6d89cee9b32c59521e1ef352adbe90dc6eb75557
andresaragonguerrero <andresaragonguerrero@gmail.com>
2026-09-25 16:13:55 +0200

    Actualizada la respuesta de los servicios

--- backend/argyros-hotel-api/Application/DTOs/ServiceResponse.cs
+++ backend/argyros-hotel-api/Application/DTOs/ServiceResponse.cs
@@ -6,6 +6,8 @@ namespace argyros_hotel_api.Application.DTOs
 
         public string Name { get; set; } = string.Empty;
 
+        public string Description { get; set; } = string.Empty;
+
         public decimal Price { get; set; }
     }
 }
